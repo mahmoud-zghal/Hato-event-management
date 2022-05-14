dad4940b19014231e59507e575f92a46189fc54f
mahmoud <mahmoud.business99@gmail.com>
2022-05-14 13:02:31 +0100

    initial commit

---
+++ src/main/java/tn/esprit/HexaCodeProject/Service/IEventService.java
@@ -0,0 +1,22 @@
+package tn.esprit.HexaCodeProject.Service;
+
+import java.util.List;
+import java.util.Map;
+
+import tn.esprit.HexaCodeProject.Entity.Event;
+
+public interface IEventService {
+
+	void addEvent(Event e);
+
+	Event UpdateEvent(Event e,Long id);
+	
+	void deleteEvent(Long id);
+	
+	List<Event> retrieveAllEvents();
+	
+	Event OverralEventRating(Long idEvent);
+	
+	List GetEventsCalendar();
+
+}
